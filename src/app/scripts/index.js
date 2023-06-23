80a086affcc3177dbeeafd90c3e77ef590cdb2f3
Sebastian Ruiz <sebasruiz15@gmail.com>
2023-06-23 15:14:03 -0500

    mend

--- src/app/scripts/index.js
+++ src/app/scripts/index.js
@@ -8,7 +8,7 @@ import { printBigPokemons } from "./printPokemons.js"
 d.addEventListener('DOMContentLoaded', async () => {
 
  await printSmallPokemons(1,5)
- await printBigPokemons(3)
+ await printBigPokemons(445)
 
 
 })
